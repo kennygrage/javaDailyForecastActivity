87025b8998d660cad6808ca2259615e01f17b87f
Kenny Grage <kennygrage@gmail.com>
2015-12-02 14:12:31 -0800

    create custom adapter

---
+++ app/src/main/java/com/example/guest/dailyforecastactivity/adapters/DayAdapter.java
@@ -0,0 +1,67 @@
+package com.example.guest.dailyforecastactivity.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.guest.dailyforecastactivity.R;
+
+/**
+ * Created by Guest on 12/2/15.
+ */
+public class DayAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private Day[] mDays;
+
+    public DayAdapter(Context context, Day[] days) {
+        mContext = context;
+        mDays = days;
+    }
+
+    @Override
+    public int getCount() {
+        return mDays.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mDays[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0; //not going to use this. This can be used to tag items for easy reference
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView === null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.daily_list_item, null);
+            holder = new ViewHolder();
+            holder.iconImageView = (ImageView) convertView.findViewById(R.id.iconImageView);
+            holder.temperatureLabel = (TextView) convertView.findViewById(R.id.temperatureLabel);
+            holder.dayLabel = (TextView) convertView.findViewById(R.id.dayNameLabel);
+
+            convertView.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        return null;
+    }
+
+    private static class ViewHolder {
+        ImageView iconImageView; //public by default
+        TextView temperatureLabel;
+        TextView dayLabel;
+    }
+}
+
