cbf38070eda1d95a0d5511e38ec4039bfc340747
Kenny Grage <kennygrage@gmail.com>
2015-12-02 10:35:23 -0800

    add adapter

--- app/src/main/java/com/example/guest/dailyforecastactivity/DailyActivityForecast.java
+++ app/src/main/java/com/example/guest/dailyforecastactivity/DailyActivityForecast.java
@@ -3,6 +3,7 @@ package com.example.guest.dailyforecastactivity;
 import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 public class DailyActivityForecast extends ListActivity {
 
@@ -10,5 +11,11 @@ public class DailyActivityForecast extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_activity_forecast);
+
+        String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                daysOfTheWeek);
+        setListAdapter(adapter);
     }
 }
